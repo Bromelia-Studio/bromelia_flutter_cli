86a16bd49f49eb715d92123aa68a2abf44a7eeba
Sergiy Vergun <sergiyvergun01@gmail.com>
2025-09-27 23:01:11 +0200

    Add navigation

--- template/flutter_app/lib/app.dart
+++ template/flutter_app/lib/app.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'core/navigation/router.dart';
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello Bromelia!'),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: router,
     );
   }
 }
---
+++ template/flutter_app/lib/core/navigation/router.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../features/home/presentation/pages/home_page.dart';
+
+final router = GoRouter(
+  initialLocation: HomePage.route,
+  routes: <RouteBase>[
+    GoRoute(
+      path: HomePage.route,
+      builder: (BuildContext context, GoRouterState state) {
+        return HomePage(key: state.pageKey);
+      },
+    ),
+  ],
+);
