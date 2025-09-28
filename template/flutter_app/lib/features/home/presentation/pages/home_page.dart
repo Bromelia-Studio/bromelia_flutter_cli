532528fe8b6ed80a980e3de5acba741678829ecc
Sergiy Vergun <sergiyvergun01@gmail.com>
2025-09-28 11:11:13 +0200

    Remove example functionality
    This commit removes the example "Flowers" functionality, including its models, data sources, repositories, Cubit, and associated UI components. The `HomePage` has been simplified to display "Hello World! 🌺". The `CommonButton` widget has also been removed.
    
    Dependencies related to this example (`json_annotation`, `json_serializable`, `bloc`, `flutter_bloc`) have been removed from `pubspec.yaml` and `pubspec.lock`.

--- template/flutter_app/lib/features/home/presentation/pages/home_page.dart
+++ template/flutter_app/lib/features/home/presentation/pages/home_page.dart
@@ -1,63 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/theme/palette.dart';
-import '../cubit/home_cubit.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  static const String route = 'flowers';
+  static const String route = 'home';
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (BuildContext context) => home_cubit()..fetchFlowers(),
-      child: Scaffold(
-        appBar: AppBar(),
-        body: BlocBuilder<home_cubit, HomeState>(
-          builder: (context, state) {
-            if(state.isLoading){
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if(state.errorMessage != null){
-              return Center(
-                child: Text(
-                  state.errorMessage!,
-                  style: TextStyle(
-                    color: context.palette.onBackground,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              );
-            }
-            return ListView.builder(
-              itemCount: state.flowers.length,
-              itemBuilder: (context, index) {
-                final flower = state.flowers[index];
-                return ListTile(
-                  leading: Text(flower.emoji, style: TextStyle(fontSize: 24),),
-                  title: Text(
-                    flower.name,
-                    style: TextStyle(
-                      color: context.palette.onBackground,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                );
-              },
-            );
-
-          },
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello World! 🌺',
+          style: TextStyle(
+            color: context.palette.onBackground,
+            fontSize: 18,
+          ),
         ),
       ),
     );
